2390b9cee5841e4eec7ede3a9e72839cf91ef96b
Depth-Vision <2975431869@qq.com>
2024-02-13 18:20:23 +0800

    CRNN_Dataset

---
+++ Optical_character_recognition/CRNN/src/utils.py
@@ -0,0 +1,30 @@
+import os
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+
+    basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    basedir = basedir.replace("\\","/")
+    raw_dir = f'{basedir}/data/raw'
+    
+    path_list = [f'{raw_dir}/{dir}' for dir in os.listdir(raw_dir)]
+
+    for i,path in enumerate(path_list):
+        mode = path.split("/")[-1]
+        if mode == "test":
+            all_list = [(f'{path_list[i]}/{dir}',dir.split('_')[0]) for dir in os.listdir(path_list[i])]
+            for (path,label) in tqdm(all_list):
+                with open('./data/labels/test.txt','a',encoding="gbk") as f:
+                    f.write(f'{path} {label}\n')
+        elif mode == "train":
+            all_list = [(f'{path_list[i]}/{dir}',dir.split('_')[0]) for dir in os.listdir(path_list[i])]
+            for (path,label) in tqdm(all_list):
+                with open('./data/labels/train.txt','a') as f:
+                    f.write(f'{path} {label}\n')
+        elif mode == "val":
+            all_list = [(f'{path_list[i]}/{dir}',dir.split('_')[0]) for dir in os.listdir(path_list[i])]
+            for (path,label) in tqdm(all_list):
+                with open('./data/labels/val.txt','a') as f:
+                    f.write(f'{path} {label}\n')
+
